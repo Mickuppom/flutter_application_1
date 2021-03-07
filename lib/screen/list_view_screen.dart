9685c0d7a4544f62b6793da28047d220f03f8e7d
xMiCx <70638947+Mickuppom@users.noreply.github.com>
2021-03-07 09:40:06 +0700

    Update list_view_screen.dart

--- lib/screen/list_view_screen.dart
+++ lib/screen/list_view_screen.dart
@@ -18,40 +18,7 @@ class _ListViewScreenState extends State<ListViewScreen> {
             "HomeScreen",
           ),
         ),
-        body: Center(
-          child: Column(
-            children: [
-              Container(
-                width: 150,
-                height: 150,
-                color: Colors.red,
-                child: Text("CAT"),
-              ),
-              Container(
-                width: 150,
-                height: 150,
-                color: Colors.amberAccent,
-                child: Image.asset(
-                  'assets/images/cat.jpg',
-                ),
-              ),
-              Container(
-                width: 150,
-                height: 150,
-                color: Colors.green,
-                child: Text("CAT"),
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text("Hit Here"),
-              ),
-              TextButton(
-                onPressed: () {},
-                child: Text("Hit Again"),
-              ),
-            ],
-          ),
-        ),
+        body: 
       ),
     );
   }
